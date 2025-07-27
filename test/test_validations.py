1337e6721929f49fd2fdda68a152c479601f8461
Mahroba <mahroba.slh@gmail.com>
2025-07-27 16:30:31 +0330

    done

--- test/test_validations.py
+++ test/test_validations.py
@@ -1,1 +1,10 @@
-from tools.validations import name_checker
+from tools import *
+
+print(name_checker(ali))
+print(name_checker(1li))
+
+print(last_name_checker(salhi))
+print(last_name_checker(sa1lhi))
+
+print(address_checker(sadi koche2))
+print(address_checker())
